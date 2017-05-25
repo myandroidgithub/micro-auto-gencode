ea42465b74d7dcf74cd292772e7a1a25cb2c7d5e
geyuankai <geyuankai1989@126.com>
2017-05-25 19:04:26 +0800

    modify MyTest

--- src/cn/javass/MyTest.java
+++ src/cn/javass/MyTest.java
@@ -31,8 +31,8 @@ public class MyTest {
 //			String [] ss2 = Parser.parse("GenConf/NeedGens/NeedGen/Params/Param$.id$").interpret(ctx);
 		}
 		
-		for (String s : ss) {
-			System.out.println("s=="+s);
+		for (String srr : ss) {
+			System.out.println("s=="+srr);
 		}
 	}
 }
